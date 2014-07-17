96004cdc52cbe6a7e26ede70c56935df2081c684
Shawn Downs <sdowns@idminc.com>
2014-07-16 21:07:07 -0400

    no message

--- chef/recipes/deployApp.rb
+++ chef/recipes/deployApp.rb
@@ -1,3 +1,29 @@
-file 'motd' do
-  content 'Yo Yo OpsWorks!'
+projectDir 		    = "/opt/#{node[:play][:project]}"
+projectPackage      = "#{node[:play][:project]}-#{node[:play][:version]}"
+gitHubURL           = node[:play][:url]
+projectUrl          = gitHubURL + "/" + projectPackage + ".zip?raw=true"
+play_user           = node[:play][:user]
+
+directory projectDir do
+  action :create
+  owner play_user
+  mode 0755
+end
+
+execute "install-project" do
+    user play_user
+    cwd projectDir
+    command <<-EOH
+    unzip #{projectPackage}.zip
+    chmod 0755 #{projectDir}/#{projectPackage}
+    EOH
+    action :nothing
+end
+
+remote_file "#{projectUrl}" do
+    source projectUrl
+    owner play_user
+    mode "0644"
+    notifies :run, "execute[install-project]", :immediately
+    action :create_if_missing
 end
