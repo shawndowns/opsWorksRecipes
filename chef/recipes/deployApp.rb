130feede4c7efa20391a708e3a7bed6981bc7fe7
Shawn Downs <sdowns@idminc.com>
2014-07-16 22:41:28 -0400

    take this!

--- chef/recipes/deployApp.rb
+++ chef/recipes/deployApp.rb
@@ -10,20 +10,9 @@ directory projectDir do
   mode 0755
 end
 
-execute "install-project" do
-    user play_user
-    cwd projectDir
-    command <<-EOH
-    unzip #{projectPackage}.zip
-    chmod 0755 #{projectDir}/#{projectPackage}
-    EOH
-    action :nothing
-end
-
-remote_file "#{projectUrl}" do
-    source projectUrl
-    owner play_user
-    mode "0644"
-    notifies :run, "execute[install-project]", :immediately
-    action :create_if_missing
-end
+user play_user
+cwd projectDir
+command <<-EOH
+unzip #{projectPackage}.zip
+chmod 0755 #{projectDir}/#{projectPackage}
+EOH
