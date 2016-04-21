f67c53b8a79e9c968181c3b8bee6bdfb61fa2334
Shawn <shawn@charis.works>
2016-04-21 17:41:20 -0400

    fixed typos and path issues for metadata.rb

--- chef/metadata.rb
+++ chef/metadata.rb
@@ -1,11 +1,11 @@
 name 				"OpsWorks Recipies"
 maintainer       	"Shawn Downs"
 maintainer_email 	"im@shawndowns.com"
-description      	"Installs/configures redis"
+description      	"Installs/configures Play Framework"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          	"1.3-M1"
 
-recipe "redis::default", "Configures s3cmd and downloads/installs the specified play dist on S3"
+recipe "recipes::deployApp", "Configures s3cmd and downloads/installs the specified play dist on S3"
 
 %w[ ubuntu ].each do |os|
   supports os
